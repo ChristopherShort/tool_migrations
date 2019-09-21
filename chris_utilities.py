a19cc307cd07d014fa90c08a735d45d2a63b0fa3
Chris Short <chrisd.short@icloud.com>
2019-09-22 09:43:59 +1000

    tidyup

--- chris_utilities.py
+++ chris_utilities.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import matplotlib as mpl
+import matplotlib.pyplot as plt
 from IPython.core.display import HTML, display_html
 
 
@@ -17,20 +18,51 @@ def clean_column_names(df, other_text_to_remove='sector'):
     clean dataframe names - replace spaces, hyphens and '_sector' for labour force data!
     '''
     if other_text_to_remove:
-        df = df.rename(columns=lambda x: x.replace(other_text_to_remove, ''))
+        df = df.rename(columns=lambda x: x.replace(other_text_to_remove, ""))
 
     df = (df
           .rename(columns=str.lower)
           .rename(columns=str.strip)
-          .rename(columns=lambda x: x.replace(' ', '_'))
-          .rename(columns=lambda x: x.replace('-', '_'))
-          .rename(columns=lambda x: x.replace('(', '_'))
-          .rename(columns=lambda x: x.replace(')', '_'))
-          .rename(columns=lambda x: x.replace('__', '_'))
-        # .rename(columns=lambda x: x.replace(, ''))
+          .rename(columns=lambda x: x.replace(" ", "_"))
+          .rename(columns=lambda x:x.replace("/", "_"))
+          .rename(columns=lambda x: x.replace("-", "_"))
+          .rename(columns=lambda x: x.replace("(", "_"))
+          .rename(columns=lambda x: x.replace(")", "_"))
+          .rename(columns=lambda x: x.replace("__", "_"))
+        # .rename(columns=lambda x: x.replace(, ""))
           )
     return df
 
+def time_delta_rule(t=12, period="A"):
+    """Return a Rule for resampling, eg t=12 returns "A-Dec
+    
+    Parameters
+    ----------
+    t : int, optional
+        The month for year-ending rule in time sereis period, by default 12
+    period: str, optional
+        Whether Annual or Quarter, optional, by default A
+    """
+    annual_dict = {
+        1: "A-Jan",
+        2: "A-Feb",
+        3: "A-Mar",
+        4: "A-Apr",
+        5: "A-May",
+        6: "A-Jun",
+        7: "A-Jul",
+        8: "A-Aug",
+        9: "A-Sept",
+        10: "A-Oct",
+        11: "A-Nov",
+        12: "A-Dec",
+    }
+
+    if period == "A":
+        return annual_dict[t]
+    else:
+        ### place holder for quarterly etc
+        return None
 
 def read_abs_data(folder_path=data_abs_path,
                   fname='310101.xls',
@@ -159,7 +191,7 @@ def read_abs_meta_data(folder_path=data_abs_path,
     if meta.iloc[:, 0].isna()[0]:
         meta.iloc[0, 0] = 'Description'
     else:
-        meta.iloc[0, 0] = met.iloc[0, 0].replace(' *> ', '', regex=True)  # wonder what ABS workbooks needed this?
+        meta.iloc[0, 0] = meta.iloc[0, 0].replace(' *> ', '', regex=True)  # wonder what ABS workbooks needed this?
 
     return meta.set_index('Series ID').rename_axis(columns=None).T
 
@@ -419,7 +451,7 @@ def write_y_axis_label(ax,
     return None
 
 
-def set_fin_year_axis(ax, rotation=0, ha='center', end_of_fin_year=True):
+def set_fin_year_axis(ax, rotation=0, ha='center', end_of_fin_year=True, every_year=True):
     '''
     Parameters:
     -----------
@@ -437,12 +469,19 @@ def set_fin_year_axis(ax, rotation=0, ha='center', end_of_fin_year=True):
         # labels_new = ['{0}-{1}'.format(i, int(i[2:])+1)
         #               if i != '' else '' for i in labels_old]
 
-        labels_new = [f'{int(t[:4]) - 1}-{t[2:4]}' if i % 2 else ''
-                      for i, t in enumerate(labels_old)]
+        labels_new = [f"{int(t[:4]) - 1}-{t[2:4]}"for t in labels_old]
     else:
         labels_new = [f"{t[:4]}-{int(t[2:4]) + 1}" for t in labels_old]
-
+    
+    
     ax.xaxis.set_ticklabels(labels_new, rotation, ha)
+    
+    if not every_year:
+        #remove every second lear
+        labels_old = [tick.get_text() for tick in ax.xaxis.get_ticklabels()]
+        labels_new = [t.get_text() if i % 2 else ""
+                      for i, t in enumerate(labels_old)]
+    
 
     return labels_new
 
@@ -532,6 +571,7 @@ def group_sum_unstack(df, group_var, sum_var, unstack_var):
        .unstack(unstack_var)
       )
 
+
 def get_oecd_members():
     oecd_members = {
         'Australia': '7-Jun-1971',
@@ -598,3 +638,149 @@ def makeTimeSeriesDataFrame():
     
     return tm.makeTimeDataFrame(freq='M')
 
+############ Matplotlib testing #########
+# See https://matplotlib.org/3.1.0/gallery/style_sheets/style_sheets_reference.html
+# See https://matplotlib.org/3.1.0/users/dflt_style_changes.html
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+
+def plot_scatter(ax, prng, nb_samples=100):
+    """Scatter plot.
+    """
+    for mu, sigma, marker in [(-.5, 0.75, 'o'), (0.75, 1., 's')]:
+        x, y = prng.normal(loc=mu, scale=sigma, size=(2, nb_samples))
+        ax.plot(x, y, ls='none', marker=marker)
+    ax.set_xlabel('X-label')
+    return ax
+
+
+def plot_colored_sinusoidal_lines(ax):
+    """Plot sinusoidal lines with colors following the style color cycle.
+    """
+    L = 2 * np.pi
+    x = np.linspace(0, L)
+    nb_colors = len(plt.rcParams['axes.prop_cycle'])
+    shift = np.linspace(0, L, nb_colors, endpoint=False)
+    for s in shift:
+        ax.plot(x, np.sin(x + s), '-')
+    ax.set_xlim([x[0], x[-1]])
+    return ax
+
+
+def plot_bar_graphs(ax, prng, min_value=5, max_value=25, nb_samples=5):
+    """Plot two bar graphs side by side, with letters as x-tick labels.
+    """
+    x = np.arange(nb_samples)
+    ya, yb = prng.randint(min_value, max_value, size=(2, nb_samples))
+    width = 0.25
+    ax.bar(x, ya, width)
+    ax.bar(x + width, yb, width, color='C2')
+    ax.set_xticks(x + width)
+    ax.set_xticklabels(['a', 'b', 'c', 'd', 'e'])
+    return ax
+
+
+def plot_colored_circles(ax, prng, nb_samples=15):
+    """Plot circle patches.
+
+    NB: draws a fixed amount of samples, rather than using the length of
+    the color cycle, because different styles may have different numbers
+    of colors.
+    """
+    for sty_dict, j in zip(plt.rcParams['axes.prop_cycle'], range(nb_samples)):
+        ax.add_patch(plt.Circle(prng.normal(scale=3, size=2),
+                                radius=1.0, color=sty_dict['color']))
+    # Force the limits to be the same across the styles (because different
+    # styles may have different numbers of available colors).
+    ax.set_xlim([-4, 8])
+    ax.set_ylim([-5, 6])
+    ax.set_aspect('equal', adjustable='box')  # to plot circles as circles
+    return ax
+
+
+def plot_image_and_patch(ax, prng, size=(20, 20)):
+    """Plot an image with random values and superimpose a circular patch.
+    """
+    values = prng.random_sample(size=size)
+    ax.imshow(values, interpolation='none')
+    c = plt.Circle((5, 5), radius=5, label='patch')
+    ax.add_patch(c)
+    # Remove ticks
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+    return ax
+
+
+def plot_histograms(ax, prng, nb_samples=10000):
+    """Plot 4 histograms and a text annotation.
+    """
+    params = ((10, 10), (4, 12), (50, 12), (6, 55))
+    for a, b in params:
+        values = prng.beta(a, b, size=nb_samples)
+        ax.hist(values, histtype="stepfilled", bins=30,
+                alpha=0.8, density=True)
+    # Add a small annotation.
+    ax.annotate('Annotation', xy=(0.25, 4.25),
+                xytext=(0.9, 0.9), textcoords=ax.transAxes,
+                va="top", ha="right",
+                bbox=dict(boxstyle="round", alpha=0.2),
+                arrowprops=dict(
+                          arrowstyle="->",
+                          connectionstyle="angle,angleA=-95,angleB=35,rad=10"),
+                )
+    return ax
+
+
+def plot_figure(style_label=""):
+    """Setup and plot the demonstration figure with a given style.
+    """
+    # Use a dedicated RandomState instance to draw the same "random" values
+    # across the different figures.
+    prng = np.random.RandomState(96917002)
+
+    # Tweak the figure size to be better suited for a row of numerous plots:
+    # double the width and halve the height. NB: use relative changes because
+    # some styles may have a figure size different from the default one.
+    (fig_width, fig_height) = plt.rcParams['figure.figsize']
+    fig_size = [fig_width * 2, fig_height / 2]
+
+    fig, axes = plt.subplots(ncols=6, nrows=1, num=style_label,
+                             figsize=fig_size, squeeze=True)
+    axes[0].set_ylabel(style_label)
+
+    plot_scatter(axes[0], prng)
+    plot_image_and_patch(axes[1], prng)
+    plot_bar_graphs(axes[2], prng)
+    plot_colored_circles(axes[3], prng)
+    plot_colored_sinusoidal_lines(axes[4])
+    plot_histograms(axes[5], prng)
+
+    fig.tight_layout()
+
+    return fig
+
+
+def plot_sample_style(add_defaults=True, style_list=['default', 'classic']):
+    # Setup a list of all available styles, in alphabetical order but
+    # the `default` and `classic` ones, which will be forced resp. in
+    # first and second position.
+    # style_list = ['default', 'classic'] + sorted(
+    #     style for style in plt.style.available if style != 'classic')
+
+    if add_defaults:
+        style_list = ['default', 'classic'] + sorted(style_list)
+    else:
+        style_list = sorted(style_list)
+    #     style for style in plt.style.available if style != 'classic')
+
+    # Plot a demonstration figure for every available style sheet.
+    for style_label in style_list:
+        with plt.style.context(style_label):
+            fig = plot_figure(style_label=style_label)
+
+    plt.show()
+
+    return fig
