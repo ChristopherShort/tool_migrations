5dd12be64571e0258768ca4c3d90850c63d86599
Chris Short <chrisd.short@icloud.com>
2019-09-22 09:54:53 +1000

    blacked

--- labor.py
+++ labor.py
@@ -10,7 +10,7 @@ DATA_FOLDER = Path.home() / "Documents/Analysis/Australian economy/Data/ABS/"
 
 
 def convert_lm7_excel_to_parquet(data_folder):
-    '''
+    """
     Read LM7.xlsx, clean and convert to parquet file
 
     Parameters
@@ -22,56 +22,63 @@ def convert_lm7_excel_to_parquet(data_folder):
     -------
 
     TODO: automatically download latest LM7 file
-    '''
+    """
 
     col_names = {
-        'Month': 'date',
-        'Sex': 'sex',
-        'Main English-speaking countries': 'MESC',
-        'Elapsed years since arrival': 'elapsed_years_since_arrival',
-        'State and territory (STT): ASGS (2011)': 'state',
-        "Employed full-time ('000)": 'employed_full_time',
-        "Employed part-time ('000)": 'employed_part_time',
-        "Unemployed looked for full-time work ('000)": 'unemployed_looked_full_time',
-        "Unemployed looked for only part-time work ('000)": 'unemployed_looked_part_time_only',
-        "Not in the labour force (NILF) ('000)": 'nilf',
+        "Month": "date",
+        "Sex": "sex",
+        "Main English-speaking countries": "MESC",
+        "Elapsed years since arrival": "elapsed_years_since_arrival",
+        "State and territory (STT): ASGS (2011)": "state",
+        "Employed full-time ('000)": "employed_full_time",
+        "Employed part-time ('000)": "employed_part_time",
+        "Unemployed looked for full-time work ('000)": "unemployed_looked_full_time",
+        "Unemployed looked for only part-time work ('000)": "unemployed_looked_part_time_only",
+        "Not in the labour force (NILF) ('000)": "nilf",
     }
 
-    osb = {'Main English-speaking countries': 'overseas',
-           'Other than main English-speaking countries': 'overseas',
-           'Australia (includes External Territories)': 'Australia',
-           'Not Stated / Inadequately Described / Born at sea': 'unknown'
-           }
+    osb = {
+        "Main English-speaking countries": "overseas",
+        "Other than main English-speaking countries": "overseas",
+        "Australia (includes External Territories)": "Australia",
+        "Not Stated / Inadequately Described / Born at sea": "unknown",
+    }
 
-    idx_labor_force = ['employed_full_time', 'employed_part_time', 'unemployed_looked_full_time',
-                       'unemployed_looked_part_time_only']
+    idx_labor_force = [
+        "employed_full_time",
+        "employed_part_time",
+        "unemployed_looked_full_time",
+        "unemployed_looked_part_time_only",
+    ]
 
-    df = (pd
-          .read_excel(data_folder / 'LM7.xlsx',
-                      usecols='A:J',
-                      sheet_name='Data 1',
-                      skiprows=3,
-                      parse_dates=[0], infer_datetime_format=True,
-                      )
-          .rename(columns=col_names)
-          .assign(date=lambda x: x.date + pd.offsets.MonthEnd(0))
-          .assign(COB=lambda x: x.MESC.map(osb))
-          .assign(labor_force=lambda x: x[idx_labor_force].sum(axis=1))
-          .assign(employed_total=lambda x: x.employed_full_time + x.employed_part_time)
-          .assign(population=lambda x: x.nilf + x.labor_force)
-          .set_index('date')
-          )
+    df = (
+        pd.read_excel(
+            data_folder / "LM7.xlsx",
+            usecols="A:J",
+            sheet_name="Data 1",
+            skiprows=3,
+            parse_dates=[0],
+            infer_datetime_format=True,
+        )
+        .rename(columns=col_names)
+        .assign(date=lambda x: x.date + pd.offsets.MonthEnd(0))
+        .assign(COB=lambda x: x.MESC.map(osb))
+        .assign(labor_force=lambda x: x[idx_labor_force].sum(axis=1))
+        .assign(employed_total=lambda x: x.employed_full_time + x.employed_part_time)
+        .assign(population=lambda x: x.nilf + x.labor_force)
+        .set_index("date")
+    )
 
-    for col in df.select_dtypes(include='object').columns:
-        df[col] = df[col].astype('category')
+    for col in df.select_dtypes(include="object").columns:
+        df[col] = df[col].astype("category")
 
-    df.to_parquet(data_folder / 'LM7.parquet')
+    df.to_parquet(data_folder / "LM7.parquet")
 
     return df
 
 
 def convert_lm5_excel_to_parquet(data_folder):
-    '''
+    """
     Read in LM5.xlsx, clean and convert to parquet file
 
     Parameters
@@ -83,69 +90,75 @@ def convert_lm5_excel_to_parquet(data_folder):
     -------
 
     TODO: automatically download latest LM5 file
-    '''
+    """
 
     col_names = {
-        'Month': 'date',
-        'Sex': 'sex',
-        'Age': 'age_group',
-        'Major country group (subcontinent) of birth: SACC (2011)': 'cob',
-        "Employed full-time ('000)": 'employed_full_time',
-        "Employed part-time ('000)": 'employed_part_time',
-        "Unemployed looked for full-time work ('000)": 'unemployed_looked_full_time',
-        "Unemployed looked for only part-time work ('000)": 'unemployed_looked_part_time_only',
-        "Not in the labour force (NILF) ('000)": 'nilf',
+        "Month": "date",
+        "Sex": "sex",
+        "Age": "age_group",
+        "Major country group (subcontinent) of birth: SACC (2011)": "cob",
+        "Employed full-time ('000)": "employed_full_time",
+        "Employed part-time ('000)": "employed_part_time",
+        "Unemployed looked for full-time work ('000)": "unemployed_looked_full_time",
+        "Unemployed looked for only part-time work ('000)": "unemployed_looked_part_time_only",
+        "Not in the labour force (NILF) ('000)": "nilf",
     }
 
-    idx_labor_force = ['employed_full_time', 'employed_part_time', 'unemployed_looked_full_time',
-                       'unemployed_looked_part_time_only']
+    idx_labor_force = [
+        "employed_full_time",
+        "employed_part_time",
+        "unemployed_looked_full_time",
+        "unemployed_looked_part_time_only",
+    ]
 
-    df = (pd
-          .read_excel(data_folder / 'LM5.xlsx',
-                      usecols='A:I',
-                      sheet_name='Data 1',
-                      skiprows=3,
-                      parse_dates=[0], infer_datetime_format=True,
-                      )
-
-          .rename(columns=col_names)
-          .assign(date=lambda x: x.date + pd.offsets.MonthEnd(0))
-          .assign(employed_total=lambda x: x.employed_full_time + x.employed_part_time)
-          .assign(labor_force=lambda x: x[idx_labor_force].sum(axis=1))
-          .assign(population=lambda x: x.nilf + x.labor_force)
-          .assign(participation_rate=lambda x: x.labor_force.divide(x.population) * 100)
-          .set_index('date')
-          )
+    df = (
+        pd.read_excel(
+            data_folder / "LM5.xlsx",
+            usecols="A:I",
+            sheet_name="Data 1",
+            skiprows=3,
+            parse_dates=[0],
+            infer_datetime_format=True,
+        )
+        .rename(columns=col_names)
+        .assign(date=lambda x: x.date + pd.offsets.MonthEnd(0))
+        .assign(employed_total=lambda x: x.employed_full_time + x.employed_part_time)
+        .assign(labor_force=lambda x: x[idx_labor_force].sum(axis=1))
+        .assign(population=lambda x: x.nilf + x.labor_force)
+        .assign(participation_rate=lambda x: x.labor_force.divide(x.population) * 100)
+        .set_index("date")
+    )
 
     # for col in df.select_dtypes(include='object').columns:
     #     df[col] = df[col].astype('category')
 
-    df.to_parquet(data_folder / 'LM5.parquet')
+    df.to_parquet(data_folder / "LM5.parquet")
 
     return df
 
 
-def read_lm5(data_folder=DATA_FOLDER, delete_unknown_COB=True):
+def read_lm5(data_folder=DATA_FOLDER, delete_unknown_COB=True, age_mapping=None):
     """[summary]
     
     Parameters
     ----------
     data_folder : [type], optional
         [description], by default DATA_FOLDER
-    remove_unknown_COB : bool, optional
+    delete_unknown_COB : bool, optional
         [description], by default True
-    
-    Returns
-    -------
-    [type]
-        [description]
+    age_mapping : [type], optional
+        [description], by default None
     """
-    df = pd.read_parquet(data_folder / 'LM5.parquet')
+
+    df = pd.read_parquet(data_folder / "LM5.parquet")
 
     if delete_unknown_COB:
-        return remove_unknown_COB(df)
-    else:
+        df = remove_unknown_COB(df)
+
+    if not age_mapping:
         return df
+    else:
+        return set_age_groups(df, age_mapping=age_mapping)
 
 
 def remove_unknown_COB(df):
@@ -157,15 +170,15 @@ def remove_unknown_COB(df):
     df : [type]
         [description]
     """
-    # Remove unknown COB observation 
-    idx = (df.COB == 'Inadequately Described / Born at Sea') | (df.COB == 'Not stated')
+    # Remove unknown COB observation
+    idx = (df.COB == "Inadequately Described / Born at Sea") | (df.COB == "Not stated")
     df = df.loc[~idx]
 
-    #rename - Australia and OS born
-    idx = df.COB == 'Australia (includes External Territories)'
+    # rename - Australia and OS born
+    idx = df.COB == "Australia (includes External Territories)"
 
-    df.loc[idx,'COB'] = 'Australian-born'
-    df.loc[~idx, 'COB'] = 'Overseas-born'
+    df.loc[idx, "COB"] = "Australian-born"
+    df.loc[~idx, "COB"] = "Overseas-born"
 
     return df
 
@@ -178,12 +191,8 @@ def male_female_population(df=None, delete_unknown_COB=True):
         df = read_lm5()
         if delete_unknown_COB:
             df = remove_unknown_COB(df)
-    
-    return (df
-        .groupby(["date", 'sex'])
-        ['labour_force', 'population'].sum()
-        .unstack()
-    )
+
+    return df.groupby(["date", "sex"])["labour_force", "population"].sum().unstack()
 
 
 def set_age_groups(df, age_mapping=None):
@@ -197,27 +206,31 @@ def set_age_groups(df, age_mapping=None):
         [description], by default None
     """
     if age_mapping is None:
-        age_mapping = {'15-19 years': '15-19 years',
-                '20-24 years': '20-24 years',
-                '25-29 years': '25-34 years',
-                '30-34 years': '25-34 years',
-                '35-39 years': '35-44 years',
-                '40-44 years': '35-44 years',
-                '45-49 years': '45-54 years',
-                '50-54 years': '45-54 years',
-                '55-59 years': '55-64 years',
-                '60-64 years': '55-64 years',
-                '65 years and over': '65 years and over'
-                }
+        age_mapping = {
+            "15-19 years": "15-19 years",
+            "20-24 years": "20-24 years",
+            "25-29 years": "25-34 years",
+            "30-34 years": "25-34 years",
+            "35-39 years": "35-44 years",
+            "40-44 years": "35-44 years",
+            "45-49 years": "45-54 years",
+            "50-54 years": "45-54 years",
+            "55-59 years": "55-64 years",
+            "60-64 years": "55-64 years",
+            "65 years and over": "65 years and over",
+        }
 
     df.age = df.age.map(age_mapping)
 
     # remove data not required for this analysis
-    return df.drop(columns=[
-        'employed_full_time', 'employed_part_time',
-        'unemployed_looked_full_time', 'unemployed_looked_part_time_only', 
-        'nilf',
-    ]
+    return df.drop(
+        columns=[
+            "employed_full_time",
+            "employed_part_time",
+            "unemployed_looked_full_time",
+            "unemployed_looked_part_time_only",
+            "nilf",
+        ]
     )
 
 
@@ -231,15 +244,13 @@ def lf_hierarchical(df=None):
         [description], by default None
     """
 
-    if not df:
+    if df is None:
         df = read_lm5()
         df = set_age_groups(df)
-    
-    return (df
-        .groupby(["date", "sex", "age", "COB"])
-        ["labour_force", "population"]
+
+    return (
+        df.groupby(["date", "sex", "age", "COB"])["labour_force", "population"]
         .sum()
         .unstack(["COB", "sex", "age"])
+        .sort_index(axis="columns")
     )
-
-
