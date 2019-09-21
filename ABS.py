6e9708efba05d2d41ad38a95e93f4c35bd57c8fb
Chris Short <chrisd.short@icloud.com>
2019-09-22 09:42:41 +1000

    Blacked

--- ABS.py
+++ ABS.py
@@ -1,8 +1,8 @@
-'''
+"""
 A set of utilites to extract and manipulate ABS data
 
 Use 'ABS' instead of 'abs' to avoid conflict with built in abs
-'''
+"""
 import re
 from pathlib import Path
 import time
@@ -20,7 +20,7 @@ DICT_FOLDER = Path.home() / "Documents/Analysis/Australian economy/Data/Dictiona
 
 
 def get_downloads_page_url(url, allow_redirects=True):
-    '''
+    """
     Get the url for the Downloads/Details Page
 
     Parameters
@@ -32,84 +32,81 @@ def get_downloads_page_url(url, allow_redirects=True):
     Returns
     -------
     The url for the download page of the latest release
-    '''
+    """
 
     session = HTMLSession()
     r = session.get(url, allow_redirects=allow_redirects)
 
-    url_details_page = [
-        url for url in r.html.absolute_links if 'DetailsPage' in url]
+    url_details_page = [url for url in r.html.absolute_links if "DetailsPage" in url]
 
     if len(url_details_page) > 1:
         print(url_details_page)
-        raise ValueError(
-            'Chris: More than 1 link found for Downloads/DetailsPage')
+        raise ValueError("Chris: More than 1 link found for Downloads/DetailsPage")
 
     return url_details_page[0]
 
 
 def download_file(first_url, data_folder=DATA_FOLDER):
     file_params = file_details(first_url)
-    file_name = file_params['filename']
+    file_name = file_params["filename"]
     session = HTMLSession()
 
     if is_file_type_downloadable(first_url):
         r = session.get(first_url)
 
-        with open(data_folder / file_name, 'wb') as output:
+        with open(data_folder / file_name, "wb") as output:
             output.write(r.content)
 
     else:
-        print('No excel or zip file contained in the url:', first_url)
+        print("No excel or zip file contained in the url:", first_url)
 
 
 def is_file_type_downloadable(url):
-    '''
+    """
     Check if the content type is an excel file
 
     Do this by looking in the content_type of the header
-    '''
+    """
 
     h = requests.head(url, allow_redirects=True)
     header = h.headers
-    content_type = header.get('content-type')
+    content_type = header.get("content-type")
 
-    if content_type.lower() in ['application/vnd.ms-excel',
-                                'application/x-zip'
-                                ]:
+    if content_type.lower() in ["application/vnd.ms-excel", "application/x-zip"]:
         return True
     return False
 
 
 def is_excel_file(url):
-    '''
+    """
     Check if the content type is an excel file
 
     Do this by looking in the content_type of the header
-    '''
+    """
     session = HTMLSession()
 
     h = session.head(url, allow_redirects=True)
     header = h.headers
-    content_type = header.get('content-type')
+    content_type = header.get("content-type")
 
-    if 'ms-excel' in content_type.lower():
+    if "ms-excel" in content_type.lower():
         return True
     return False
 
 
 def file_details(url_table):
-    file_details_labels = ['agent',
-                           'filename',
-                           'catalog_no',
-                           'data_type',
-                           'unknow_param_0',
-                           'unknow_param_1',
-                           'data_date',
-                           'release_date',
-                           'version',
-                           ]
-    file_details_parse = urlparse(url_table).query.split('&')
+    file_details_labels = [
+        "agent",
+        "filename",
+        "catalog_no",
+        "data_type",
+        "unknow_param_0",
+        "unknow_param_1",
+        "data_date",
+        "release_date",
+        "version",
+    ]
+    file_details_parse = urlparse(url_table).query.split("&")
 
     file_params = dict(zip(file_details_labels, file_details_parse))
 
@@ -117,21 +114,21 @@ def file_details(url_table):
 
 
 def get_file_name_from_url(url):
-    m = re.search(r'log\\?openagent&(\d+\\.xls)', url)
+    m = re.search(r"log\\?openagent&(\d+\\.xls)", url)
 
     if m is None:
-        raise ValueError('Chris: filename not found in url')
+        raise ValueError("Chris: filename not found in url")
     else:
         return m.group(1)
 
 
 def make_true_hiearchical(df, index_names=None):
-    '''
+    """
     ABS often places a hierarchical index in one column
     Create pandas hiearchical by joining a duplicated index that has full names of the
     level 0 hiearchy joined with original index
     and removing superfluous rows
-    '''
+    """
 
     # use copy to ensure df_hist index not interfered with
     outer = pd.Series(df.index.values).copy()
@@ -163,7 +160,7 @@ def make_true_hiearchical(df, index_names=None):
 
 
 def gen_ABS_3412(file_path, calendar=None):
-    '''
+    """
     A generator to read in ABS Migration Australia data
 
     Parameters:
@@ -172,70 +169,78 @@ def gen_ABS_3412(file_path, calendar=None):
     calendar: boolean
         True for calendar year data, False for financial year data[]
 
-    '''
+    """
 
     if not isinstance(calendar, bool):
-        raise ValueError(
-            'Chris: boolean for calendar or financial year not set')
+        raise ValueError("Chris: boolean for calendar or financial year not set")
 
     sheets_dict = pd.read_excel(file_path, sheet_name=None)
 
     for sheet_name, df_ in sheets_dict.items():
 
         # Ignore contents and other non-data pages
-        if 'Table' not in sheet_name:
+        if "Table" not in sheet_name:
             continue
 
         # Table descriptor in 4th row of worksheet - 3rd row of dataframe
 
-        table = re.match(r'Table_* * \d\.\d+', df_.iloc[3, 0]).group()
+        table = re.match(r"Table_* * \d\.\d+", df_.iloc[3, 0]).group()
 
         if calendar:
-            year_string = re.search(r'\d\d\d\d', df_.iloc[3, 0]).group()
+            year_string = re.search(r"\d\d\d\d", df_.iloc[3, 0]).group()
             year_int = int(year_string)
         else:
-            year_string = re.search(r'\d\d\d\d-\d\d', df_.iloc[3, 0]).group()
-            year_int = int('20' + year_string[-2:])
+            year_string = re.search(r"\d\d\d\d-\d\d", df_.iloc[3, 0]).group()
+            year_int = int("20" + year_string[-2:])
 
         print(year_string, table)
 
-        df_.columns = ['state', 'major_groupings',
-                       'minor_groupings', 'nom_arrival', 'nom_departure', 'nom']
+        df_.columns = [
+            "state",
+            "major_groupings",
+            "minor_groupings",
+            "nom_arrival",
+            "nom_departure",
+            "nom",
+        ]
 
         # strip descriptor rows at top of worksheet
         df_ = df_.iloc[5:].copy()
 
-        df_.loc[:, 'state'] = df_['state'].fillna(method='ffill')
-        df_.loc[:, 'major_groupings'] = df_['major_groupings'].fillna(method='ffill')
-        df_.loc[:, 'minor_groupings'] = df_.loc[:,
-                                                'minor_groupings'].fillna(value='Total')
-        df_ = df_.dropna(axis='rows', thresh=4)
-        df_.state = df_.state.str.replace(r'Australia\([^\)]\)', 'Australia', regex=True)
+        df_.loc[:, "state"] = df_["state"].fillna(method="ffill")
+        df_.loc[:, "major_groupings"] = df_["major_groupings"].fillna(method="ffill")
+        df_.loc[:, "minor_groupings"] = df_.loc[:, "minor_groupings"].fillna(
+            value="Total"
+        )
+        df_ = df_.dropna(axis="rows", thresh=4)
+        df_.state = df_.state.str.replace(
+            r"Australia\([^\)]\)", "Australia", regex=True
+        )
 
         if calendar:
-            df_['year'] = pd.datetime(year_int, 12, 31)
+            df_["year"] = pd.datetime(year_int, 12, 31)
         else:
-            df_['year'] = pd.datetime(year_int, 6, 30)
+            df_["year"] = pd.datetime(year_int, 6, 30)
 
         yield df_
 
 
 def strip_footnote_marks(df, col_name=None, is_index=False):
-    '''
+    """
     ABS columns (often states) have footnotes (eg (a), (b) etc)
     Remove them
-    '''
+    """
 
     if is_index:
-        df.index = df.index.str.replace(r' (.\)', '').str.strip()
+        df.index = df.index.str.replace(r" (.\)", "").str.strip()
     else:
-        df[col_name] = df[col_name].str.replace(r' (.\)', '').str.strip()
+        df[col_name] = df[col_name].str.replace(r" (.\)", "").str.strip()
 
     return df
 
 
 def make_year_date(df_index, is_calendar=True):
-    '''
+    """
     For certain historical data, ABS often only supplies the year number, not a date
     Convert index to a time stamp
 
@@ -248,7 +253,7 @@ def make_year_date(df_index, is_calendar=True):
 
     is_calendar: boolean
       True for calendar year, False for financial year
-    '''
+    """
     # assert df_index is ?  how to check a
     if is_calendar:
         df_index = [pd.datetime(year, 12, 31) for year in df_index]
@@ -259,29 +264,28 @@ def make_year_date(df_index, is_calendar=True):
 
 
 def download_abs_file(url, xl_file_name, data_folder=DATA_FOLDER):
-    '''
+    """
     Download the excel file given by the url
-    '''
+    """
     session = HTMLSession()
 
     if is_file_type_downloadable(url):
         xl_file = session.get(url)
-        with open(data_folder / xl_file_name, 'wb') as output:
+        with open(data_folder / xl_file_name, "wb") as output:
             output.write(xl_file.content)
     else:
-        raise ValueError(f'Chris - Not valide excel file: {url}, {xl_file_name}.')
+        raise ValueError(f"Chris - Not valide excel file: {url}, {xl_file_name}.")
 
     # print(f'{file_name} donwloaded.')
     return None
 
 
-def download_abs_catalog_excel_files(cat_no='3101.0',
-    url_cat_downloads_page=None,
-    data_folder=DATA_FOLDER
-    ):
-    '''
+def download_abs_catalog_excel_files(
+    cat_no="3101.0", url_cat_downloads_page=None, data_folder=DATA_FOLDER
+):
+    """
     Download all excel files associated with a given catalog number
-    '''
+    """
     print(url_cat_downloads_page)
     session = HTMLSession()
 
@@ -293,7 +297,7 @@ def download_abs_catalog_excel_files(cat_no='3101.0',
     excel_downloads_page = session.get(url_cat_downloads_page)
 
     # all downloads are tr elements of class 'listentry'
-    links_list = excel_downloads_page.html.find('tr.listentry')
+    links_list = excel_downloads_page.html.find("tr.listentry")
 
     # pattern to find excel links - eg 31010do003_200106.xls
     pat = r"log\?openagent&([^\.]+\.xls)"
@@ -322,7 +326,7 @@ def download_abs_catalog_excel_files(cat_no='3101.0',
 
 
 def ASGS_definitions(dict_folder=DICT_FOLDER):
-    '''
+    """
     Return a dataframe containing ASGS 2016 links
 
     Parameters:
@@ -332,24 +336,25 @@ def ASGS_definitions(dict_folder=DICT_FOLDER):
     -------
     asgs: a datafram of all ASGS regions
     asgs_mapper: a pandas Series mapping ASGS codes (index) to ASGS names
-    '''
-    asgs = pd.read_csv(dict_folder / 'SA2_2016_AUST.csv')
+    """
+    asgs = pd.read_csv(dict_folder / "SA2_2016_AUST.csv")
 
-    code_2_name_dict = {'SA2_MAINCODE_2016': 'SA2_NAME_2016',
-                        'SA3_CODE_2016': 'SA3_NAME_2016',
-                        'SA4_CODE_2016': 'SA4_NAME_2016',
-                        'GCCSA_CODE_2016': 'GCCSA_NAME_2016',
-                        'STATE_CODE_2016': 'STATE_NAME_2016'
-                        }
+    code_2_name_dict = {
+        "SA2_MAINCODE_2016": "SA2_NAME_2016",
+        "SA3_CODE_2016": "SA3_NAME_2016",
+        "SA4_CODE_2016": "SA4_NAME_2016",
+        "GCCSA_CODE_2016": "GCCSA_NAME_2016",
+        "STATE_CODE_2016": "STATE_NAME_2016",
+    }
 
     def gen_make_code_mapper(code_2_name_dict, asgs):
         for code, name in code_2_name_dict.items():
-            code_map = (asgs
-                        .set_index(code, drop=True)[name]
-                        .drop_duplicates()
-                        .rename_axis('code')
-                        .rename('name')
-                        )
+            code_map = (
+                asgs.set_index(code, drop=True)[name]
+                .drop_duplicates()
+                .rename_axis("code")
+                .rename("name")
+            )
             code_map.index = code_map.index.astype(str)
             yield code_map
 
@@ -358,30 +363,59 @@ def ASGS_definitions(dict_folder=DICT_FOLDER):
     return asgs, asgs_mapper
 
 
+def get_state_gccsa_dict(asgs=None):
+    if asgs is None:
+        asgs, asgs_mapper = ASGS_definitions()
+
+    gccsa_state_dict = (
+        asgs[["GCCSA_NAME_2016", "STATE_NAME_2016"]]
+        .drop_duplicates()
+        .set_index("GCCSA_NAME_2016")
+        .squeeze()
+        .to_dict()
+    )
+
+    other_caps_dict = {
+        "Sydney": "New South Wales",
+        "Melbourne": "Victoria",
+        "Brisbane": "Queensland",
+        "Adelaide": "South Australia",
+        "Perth": "Western Australia",
+        "Hobart": "Tasmania",
+        "Darwin": "Northern Territory",
+        "Canberra": "Australian Capital Territory",
+        "Total": "Total",
+        "total": "total",
+    }
+
+    return {**gccsa_state_dict, **other_caps_dict}
+
+
 def pop_component_definitions():
-    component_2_name_dict = {'1': 'births',
-                             '2': 'deaths',
-                             '3': 'natural_increase',
-                             '4': 'nim_arrivals',
-                             '5': 'nim_departures',
-                             '6': 'nim',
-                             '7': 'nom_arrivals',
-                             '8': 'nom_departures',
-                             '9': 'nom',
-                             '10': 'erp'
-                             }
+    component_2_name_dict = {
+        "1": "births",
+        "2": "deaths",
+        "3": "natural_increase",
+        "4": "nim_arrivals",
+        "5": "nim_departures",
+        "6": "nim",
+        "7": "nom_arrivals",
+        "8": "nom_departures",
+        "9": "nom",
+        "10": "erp",
+    }
     return component_2_name_dict
 
 
 def index_to_datetime(df):
-    df.index = pd.to_datetime(df.index.astype(str) + '-06-30')
-    df.index.name = 'date'
+    df.index = pd.to_datetime(df.index.astype(str) + "-06-30")
+    df.index.name = "date"
     return df
 
 
 def drop_columns(df, columns):
     df.columns = df.columns.droplevel(columns)
-    return(df)
+    return df
 
 
 def drop_level(df, levels):
@@ -389,29 +423,29 @@ def drop_level(df, levels):
     return df
 
 
-def abs_stat_sdmx(sdmx='ABS_ANNUAL_ERP_ASGS2016', drop_levels=['FREQUENCY', 'MEASURE']):
-    '''
+def abs_stat_sdmx(sdmx="ABS_ANNUAL_ERP_ASGS2016", drop_levels=["FREQUENCY", "MEASURE"]):
+    """
     Return the ERP for all ASGS levels SA2 and above via ABS SDMX interface
 
     Parameters
     ----------
     sdmx: str, sdmx dataset to extract
     drop_levels: list, levels to drop from column multi-index returned by SDMX
-    '''
+    """
 
-    ABS_request = Request('ABS')
+    ABS_request = Request("ABS")
 
-    df = (ABS_request
-          .data(sdmx)
-          .write()
-          .pipe(index_to_datetime)
-          .pipe(drop_level, levels=drop_levels)
-          .sort_index(axis='index')
-          .sort_index(axis='columns')
-          .unstack()
-          .rename('value')
-          .reset_index()
-          )
+    df = (
+        ABS_request.data(sdmx)
+        .write()
+        .pipe(index_to_datetime)
+        .pipe(drop_level, levels=drop_levels)
+        .sort_index(axis="index")
+        .sort_index(axis="columns")
+        .unstack()
+        .rename("value")
+        .reset_index()
+    )
 
     return df
 
@@ -420,7 +454,7 @@ def abs_stat_sdmx(sdmx='ABS_ANNUAL_ERP_ASGS2016', drop_levels=['FREQUENCY', 'MEA
 
 
 def get_url_dict(url_downloads_page):
-    '''
+    """
     Create a dictionary containing the table name and url of all downloadable excel files.
 
     The order of the dict is the order is implicilty the table order on the page
@@ -434,7 +468,7 @@ def get_url_dict(url_downloads_page):
     -------
     url_dict: dictionary
       the key is the table name, the value is the url
-    '''
+    """
 
     # TODO: reassess whether download excel or zip
     # if zip, need to upack prior to storing
@@ -446,20 +480,20 @@ def get_url_dict(url_downloads_page):
 
     # each row in the html table that contains an ABS table  has class attribute 'listentry'
     # find all such rows
-    tables = r.html.find('tr.listentry')
+    tables = r.html.find("tr.listentry")
     for t in tables:
         # only want the links associated with each ABS table
-        if t.text.lower().startswith('table'):
+        if t.text.lower().startswith("table"):
             # the links in each row are both zip, and xls - only want xls
             for link in t.absolute_links:
-                if '.xls' in link:
+                if ".xls" in link:
                     url_dict[t.text] = link
 
     return url_dict
 
 
 def get_table_url(url, file_name):  # eg 310101 or LM5
-    '''
+    """
     Get the url for a Table, eg Table 1 from 3101 or LM5 from 6291 etc,
     from the Downloads page, the relevant catelog item
 
@@ -474,7 +508,7 @@ def get_table_url(url, file_name):  # eg 310101 or LM5
     ------
     links[0]: str  (first, and only element from a filtered list comprehension of URLs)
         The url of the data for the relevant file
-    '''
+    """
     file_name = file_name.lower()
 
     session = HTMLSession()
@@ -484,7 +518,8 @@ def get_table_url(url, file_name):  # eg 310101 or LM5
 
     if len(links) > 1:
         raise ValueError(
-            f'Chris: Expected unique link, but found than 1 link found: {links}')
+            f"Chris: Expected unique link, but found than 1 link found: {links}"
+        )
 
     return links
 
