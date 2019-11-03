df19e1d2d06cf0f011a1aa89ce05979fb2644762
Chris Short <chrisd.short@icloud.com>
2019-11-03 12:36:21 +1100

    add plot of lines from current style cycler

--- chris_utilities.py
+++ chris_utilities.py
@@ -459,6 +459,44 @@ def makeTimeSeriesDataFrame():
 
 ############ Matplotlib testing #########
 # See https://matplotlib.org/3.1.0/gallery/style_sheets/style_sheets_reference.html
+
+
+# Plot current style colors for white and dark backgrounds
+
+def plot_current_style_lines():
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = prop_cycle.by_key()['color']
+
+    lwbase = plt.rcParams['lines.linewidth']
+    thin = lwbase / 2
+    thick = lwbase * 3
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
+    for icol in range(2):
+        if icol == 0:
+            lwx, lwy = thin, lwbase
+        else:
+            lwx, lwy = lwbase, thick
+        for irow in range(2):
+            for i, color in enumerate(colors):
+                axs[irow, icol].axhline(i, color=color, lw=lwx)
+                axs[irow, icol].axvline(i, color=color, lw=lwy)
+
+        axs[1, icol].set_facecolor('k')
+        axs[1, icol].xaxis.set_ticks(np.arange(0, 10, 2))
+        axs[0, icol].set_title('line widths (pts): %g, %g' % (lwx, lwy),
+                            fontsize='medium')
+
+    for irow in range(2):
+        axs[irow, 0].yaxis.set_ticks(np.arange(0, 10, 2))
+
+    fig.suptitle('Colors in the current prop_cycle', fontsize='large')
+
+    return #fig
+
+
+
+
 # See https://matplotlib.org/3.1.0/users/dflt_style_changes.html
 
 # Fixing random state for reproducibility
