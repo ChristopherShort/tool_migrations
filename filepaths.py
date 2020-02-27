a0311caaf1ad598e359c279af8980891bdf16b9f
Chris Short <chrisd.short@icloud.com>
2020-02-28 10:46:58 +1100

    Add filepaths

---
+++ filepaths.py
@@ -0,0 +1,27 @@
+"""
+File paths to data
+"""
+
+
+# the data storage
+base_data_folder = Path.home() / "Analysis/Australian economy/Data"
+
+
+dict_data_folder = base_data_folder / "Dictionaries"
+
+abs_data_folder = base_data_folder / "ABS"
+
+# Home Affairs stock data
+stock_data_folder = base_data_folder / "Stock"
+
+#NOM unit record
+unit_record_folder = base_data_folder / "NOM unit record data"
+individual_movements_folder = unit_record_folder / "NOM individual movements"
+abs_nom_propensity = unit_record_folder / "ABS propensity"
+abs_nom_data_parquet_folder = unit_record_folder / "Traveller Characteristics Parquet"
+
+# Grant data
+grant_data_folder = base_data_folder / "Grant"
+
+#visa_data
+program_data_folder = base_data_folder / "Visa"
